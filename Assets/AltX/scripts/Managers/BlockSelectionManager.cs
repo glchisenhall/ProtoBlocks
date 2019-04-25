48dc1fd0ed9c0e49dfdf0b153bc71414863fd9fe
Topher Lee <topher.lee.13@gmail.com>
2019-04-25 16:19:17 -0400

    Project Push #1 (WIP)

---
+++ Assets/AltX/scripts/Managers/BlockSelectionManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSelectionManager : MonoBehaviour
+{
+
+    public GameObject blockSurface;
+    //[HideInInspector]
+    public Collider col;
+    //[HideInInspector]
+    public Material defaultMaterial;
+    //[HideInInspector]
+    public Material highlightMaterial;
+
+    public Transform trans;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = blockSurface.GetComponent<Collider>();
+        defaultMaterial = blockSurface.GetComponent<Renderer>().material;
+        trans = transform;
+}
+    private void OnMouseEnter()
+    {
+        blockSurface.GetComponent<Renderer>().material = highlightMaterial;
+    }
+    private void OnMouseExit()
+    {
+        blockSurface.GetComponent<Renderer>().material = defaultMaterial;
+    }
+    private void OnMouseDown()
+    {
+        //BuildManager.PlaceSelectedBlock(trans.y + 2f)
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
