b96ba9bc4f73144479d4791176bef6153287957b
GChisenhall <gchisenhall33@gmail.com>
2019-04-26 10:18:59 -0400

    Massive Changes Bruh!...

--- Assets/AltX/scripts/Managers/BlockList.cs
+++ Assets/AltX/scripts/Managers/BlockList.cs
@@ -1,18 +1,64 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
-public class BlockList : MonoBehaviour
+namespace AltX.scripts
 {
-    // Start is called before the first frame update
-    void Start()
+    public class BlockList
     {
-        
-    }
+        public List<Block> blocks = new List<Block>();
+        public Block[] aGameObjects;
+        public struct Block
+        {
+            public GameObject obj;
+            public int pegCount;
+            public int height;
+            public Block(GameObject obj, int pegCount, int height)
+            {
+                this.obj = obj;
+                this.pegCount = pegCount;
+                this.height = height;
+            }
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public void CreateBlock(GameObject obj, int pegCount, int height)
+        {
+            // Create new block
+            Block b = new Block
+            {
+                obj = obj,
+                pegCount = pegCount,
+                height = height
+            };
+            // Add new block to List
+            AddBlockToList(b);
+            aGameObjects =  ListBlocks().ToArray();
+        }
+
+        void AddBlockToList(Block b)
+        {
+            blocks.Add(b);
+        }
+
+        public List<Block> ListBlocks()
+        {
+            return blocks;
+        }
+        public void ClearBlocks()
+        {
+            aGameObjects = null;
+            blocks = new List<Block>();
+        }
+        public void PegCounter()
+        {
+            int count = 0;
+            if(aGameObjects != null)
+            foreach (Block m in aGameObjects)
+            {
+                count += m.pegCount;
+            }
+            Debug.Log(count);
+        }
     }
 }
