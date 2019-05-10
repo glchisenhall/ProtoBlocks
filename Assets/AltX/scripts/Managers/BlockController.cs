511ff93320f4afeeec8c71e1aaa9699b31ae4864
GChisenhall <gchisenhall33@gmail.com>
2019-05-10 03:25:52 -0400

    Alt-X namespace fixes

---
+++ Assets/AltX/scripts/Managers/BlockController.cs
@@ -0,0 +1,34 @@
+﻿using PCPi.scripts.Managers;
+using UnityEngine;
+
+namespace AltX.scripts.Managers
+{
+    /// <summary>
+    /// BlockController Class
+    /// </summary>
+    public class BlockController : BlockSettings
+    {
+        private void OnMouseDown()
+        {
+            BlockSpawnManager.PlaceSelectedBlock(BlockToSpawn, transform.position + transform.position, transform);
+        }
+        /// <summary>
+        /// Activates highlighter material & assigns block to spawn
+        /// </summary>
+        private void OnMouseEnter()
+        {
+            if (BlockManager)
+            {
+                BlockToSpawn = BlockManager.GetComponent<BlockManager>().GetSelectedBlock();
+            }
+            gameObject.GetComponent<Renderer>().material = highlightMaterial;
+        }
+        /// <summary>
+        /// Deactivates highlighter material
+        /// </summary>
+        private void OnMouseExit()
+        {
+            gameObject.GetComponent<Renderer>().material = defaultMaterial;
+        }
+    }
+}
---
+++ Assets/AltX/scripts/Managers/BlockSpawnManager.cs
@@ -0,0 +1,36 @@
+﻿/// <summary>
+/// Project: ProtoBlock Builder
+/// Made for use in Projects: Unicorn Snake & Protoblocks (...more to come?)
+/// Filename: BlockSpawnManager.cs
+/// Created by PCPi & AltX
+/// Written by: Gary Chisenhall
+/// </summary>
+#region /// USING
+using UnityEngine;
+#endregion
+namespace AltX.scripts.Managers
+{
+    public class BlockSpawnManager : MonoBehaviour
+    {
+        public void BlockDestruct(GameObject gameObject)
+        {
+                Destroy(gameObject);
+        }
+        public static void PlaceSelectedBlock(GameObject blockToSpawn, Vector3 pos, Transform parent)
+        {
+            if (blockToSpawn != null)
+            {
+                Transform b = Instantiate<GameObject>(blockToSpawn, pos, new Quaternion(0f,0f,0f,0f), parent).transform;
+                b.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+            }
+        }
+        public static void PlaceBaseBlock(GameObject baseBlock)
+        {
+            if (baseBlock != null)
+            {
+                Transform b = Instantiate<GameObject>(baseBlock, new Vector3(0f, 0f, 0f), new Quaternion(0f, 0f, 0f, 0f)).transform;
+                b.localScale = new Vector3(1f, 1f, 1f);
+            }
+        }
+    }
+}
